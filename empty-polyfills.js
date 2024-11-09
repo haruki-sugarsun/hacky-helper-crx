090f1bb73b19eed7705540148bbf09084369770c
Haruki Sato <haruki-sugarsun@users.noreply.github.com>
2024-11-09 13:35:21 +0900

    [hacky-helper] add missing files.

---
+++ empty-polyfills.js
@@ -0,0 +1,9 @@
+export function rm (path) { return null }
+
+// fs
+export function fs () {}
+export function promises () {}
+export function createReadStream () {}
+
+export default fs ();
+// etc...
