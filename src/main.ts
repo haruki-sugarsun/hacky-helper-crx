bab172c6d1e8f17f55eb9949ac8a512747f7dff7
Haruki Sato <haruki-sugarsun@users.noreply.github.com>
2024-11-11 13:26:12 +0900

    [hacky-helper] Fix reference to the missing files.

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
+// import typescriptLogo from './typescript.svg'
+// import viteLogo from '/vite.svg'
 // import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      <!-- <img src="$_{viteLogo}" class="logo" alt="Vite logo" /> -->
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+      <!-- <img src="$_{typescriptLogo}" class="logo vanilla" alt="TypeScript logo" /> -->
     </a>
     <h1>Vite + TypeScript</h1>
     <div class="card">
